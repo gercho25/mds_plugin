b8955a7b72a69fb7c4131b8004d6d3227a67d890
gercho25 <germanesalinas@gmail.com>
2016-12-30 13:17:38 -0300

    modif

--- www/serviceDiscovery.js
+++ www/serviceDiscovery.js
@@ -1,6 +1,9 @@
 var serviceDiscovery = {
     getNetworkServices:function(service, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "serviceDiscovery", "getNetworkServices", [service]);
+    },
+	getConnectedSSID:function(service, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "serviceDiscovery", "getConnectedSSID", [service]);
     }
 };
 
